5721cfbd00ecc8c653eb49696fdafd1450f5313a
HASSANnu-source <nuraianyhassan@gmail.com>
2025-09-11 17:00:29 +0330

    fix: scrollbar issue

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,7 +2,7 @@ import Board from '../components/Board'
 
 const Game = () => {
   return (
-    <div className='flex-center min-h-screen m-3'>
+    <div className='flex-center min-h-screen'>
         <Board />
     </div>
   )
